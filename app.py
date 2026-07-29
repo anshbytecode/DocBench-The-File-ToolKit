f206deee66fa2a10eb90bcda2850e671377dc144
Anshul Bhilare <terminatoranshul@gmail.com>
2026-07-29 16:44:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask import Flask, render_template, request, send_file, jsonify, abort
 from utils import pdf_tools, image_tools, office_tools
 app = Flask(__name__)
 app.config["MAX_CONTENT_LENGTH"] = 20 * 1024 * 1024  
+
 TOOLS = {
     "merge-pdf": {
         "name": "Merge PDF", "category": "Organize",
