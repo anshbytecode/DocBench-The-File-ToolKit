ae1629a2669545132b7ef5f848ac57f8f35baaea
Anshul Bhilare <terminatoranshul@gmail.com>
2026-08-24 19:27:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import io
 import os
 import traceback
 from flask import Flask, render_template, request, send_file, jsonify, abort
+
+
 from utils import pdf_tools, image_tools, office_tools
 app = Flask(__name__)
 
